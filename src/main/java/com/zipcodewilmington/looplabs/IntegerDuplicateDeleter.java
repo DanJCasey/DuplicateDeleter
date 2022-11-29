4214bfcc29bb46c28c045b256fe11028311fddd6
Daniel <daniel@Zipcoders-MacBook-Pro-10.local>
2022-11-29 17:35:59 -0500

    all tests passing

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,54 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+    public int countNumberOfOccurrences(int number){
+        int count = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] == number){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        int numberOfElements = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(countNumberOfOccurrences(array[i]) < maxNumberOfDuplications){
+                numberOfElements++;
+            }
+        }
+        int tracker = 0;
+        Integer[] newArray = new Integer[numberOfElements];
+        for(int j = 0; j < array.length; j++){
+            if(countNumberOfOccurrences(array[j]) < maxNumberOfDuplications){
+                newArray[tracker] = array[j];
+                tracker++;
+            }
+        }
+        return newArray;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int numberOfElements = 0;
+        for(int i = 0; i < array.length; i++){
+            if(countNumberOfOccurrences(array[i]) != exactNumberOfDuplications){
+                numberOfElements++;
+            }
+        }
+        int tracker = 0;
+        Integer[] newArray = new Integer[numberOfElements];
+        for(int j = 0; j < array.length; j++){
+            if (countNumberOfOccurrences(array[j]) != exactNumberOfDuplications){
+                newArray[tracker] = array[j];
+                tracker++;
+            }
+        }
+        return  newArray;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -5,4 +5,54 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+    public int countNumberOccurrences(String value) {
+        int count = 0;
+        for(int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        int numberOfElements = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(countNumberOccurrences(array[i]) < maxNumberOfDuplications) {
+                numberOfElements++;
+            }
+        }
+        int tracker = 0;
+        String[] newArray = new String[numberOfElements];
+        for(int j = 0; j < array.length; j++){
+            if(countNumberOccurrences(array[j]) < maxNumberOfDuplications) {
+                newArray[tracker] = (array[j]);
+                tracker++;
+            }
+        }
+        return newArray;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int numberOfElements = 0;
+        for(int i = 0; i < array.length; i++){
+            if(countNumberOccurrences(array[i]) != exactNumberOfDuplications){
+                numberOfElements++;
+            }
+        }
+        int tracker = 0;
+        String[] newArray = new String[numberOfElements];
+        for (int j = 0; j < array.length; j++){
+            if(countNumberOccurrences(array[j]) != exactNumberOfDuplications) {
+                newArray[tracker] = array[j];
+                tracker++;
+            }
+        }
+        return newArray;
+    }
 }
